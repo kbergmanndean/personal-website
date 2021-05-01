685b74936537e6d88a62bb2d60939faed075f281
Kaia Bergmann-Dean <kbergmanndean@gmail.com>
2021-05-01 15:00:02 -0400

    fixed javascript error

--- script.js
+++ script.js
@@ -1,3 +1,3 @@
 const h2=document.createElement('h2');
-h2.textcontent="I can't believe I'm a pizzeria waittress when all I've ever wante to be was a pop star.";
+h2.textContent=`"I can't believe I'm a pizzeria waittress when all I've ever wanted to be was a pop star."`;
 document.querySelector('body').appendChild(h2);
