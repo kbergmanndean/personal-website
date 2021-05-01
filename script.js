f5d6f2550982f0f555711fe1728dd568eae1a08d
Kaia Bergmann-Dean <kbergmanndean@gmail.com>
2021-05-01 14:53:07 -0400

    add script.js, connect to index.html

---
+++ script.js
@@ -0,0 +1,3 @@
+const h2=document.createElement('h2');
+h2.textcontent="I can't believe I'm a pizzeria waittress when all I've ever wante to be was a pop star.";
+document.querySelector('body').appendChild(h2);
