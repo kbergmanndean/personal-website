57bc376b0bcca5ec1b5650d2c1debc5493ced62a
Kaia Bergmann-Dean <kbergmanndean@gmail.com>
2021-05-02 19:07:19 -0400

    added cherubs

--- script.js
+++
@@ -1,3 +0,0 @@
-const h2=document.createElement('h2');
-h2.textContent=`"I can't believe I'm a pizzeria waittress when all I've ever wanted to be was a pop star."`;
-document.querySelector('body').appendChild(h2);
