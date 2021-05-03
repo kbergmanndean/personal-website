e3d77f1af75293fc93fadb4ebd25db6f0287abba
Kaia Bergmann-Dean <kbergmanndean@gmail.com>
2021-05-03 18:34:14 -0400

    added first art slide, began js

---
+++ script.js
@@ -0,0 +1,11 @@
+const slide1=document.getElementById('slideWyeth');
+
+const slide2=document.createElement('div')
+
+const slideShow=document.getElementById('art')
+
+document.body.appendChild(slide2)
+
+
+
+
