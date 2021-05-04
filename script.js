99d00548853cb1f0bb7d0e427c23f0c6bb07c126
Kaia Bergmann-Dean <kbergmanndean@gmail.com>
2021-05-04 12:38:12 -0400

    finished slideshow

--- script.js
+++ script.js
@@ -1,14 +1,21 @@
 let i=0;
 let slides=[];
 let time=3500;
+let captions=[];
+let artist=document.getElementById("artist")
 
 slides[0]="wyeth.jpeg"
 slides[1]="darger.jpeg"
 slides[2]="basquiat.jpg"
 slides[3]="bruegel.jpeg"
+captions[0]="Andrew Wyeth"
+captions[1]="Henry Darger"
+captions[2]="Jean-Michel Basquiat"
+captions[3]="Pieter Bruegel the Elder"
 
 function nextSlide(){
     document.slide.src=slides[i];
+    artist.textContent=captions[i]
     if(i<slides.length-1) {
         i++;
     } else {
@@ -18,6 +25,11 @@ function nextSlide(){
 }
 
 window.onload=nextSlide;
+
+
+
+
+
 
 
 
